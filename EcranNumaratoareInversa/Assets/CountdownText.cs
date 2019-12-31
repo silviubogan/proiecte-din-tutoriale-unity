8e21f9eb2a21b25a9a8539531b039e675cfdde9e
Silviu Bogan <silviubogan@gmail.com>
2019-12-31 19:32:32 +0200

    Added new tutorial

---
+++ EcranNumaratoareInversa/Assets/CountdownText.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class CountdownText : MonoBehaviour
+{
+    public int startSeconds = 3;
+    public GameObject toBeShownAfter;
+
+    Text countdownText;
+
+    void Awake()
+    {
+        countdownText = GetComponent<Text>();
+    }
+
+    void OnEnable()
+    {
+        // aici eventual reseteaza starea jocului in caz ca nivelul
+        // anterior jucat este inca vizibil pe ecran in spatele de
+        // exemplu al meniurilor:
+        toBeShownAfter.SetActive(false);
+
+        countdownText.text = startSeconds.ToString();
+        countdownText.enabled = true;
+
+        StartCoroutine(Countdown());
+    }
+
+    IEnumerator Countdown()
+    {
+        for (int i = startSeconds; i >= 1; --i)
+        {
+            countdownText.text = i.ToString();
+            StartCoroutine(FadeTextToFullAlpha(1f, countdownText));
+
+            yield return new WaitForSeconds(1);
+        }
+
+        // aici se apeleaza cod sa fie executat dupa ce s-a ajuns la 0:
+        gameObject.SetActive(false);
+        toBeShownAfter.SetActive(true);
+    }
+
+    IEnumerator FadeTextToFullAlpha(float t, Text txt)
+    {
+        txt.color = new Color(txt.color.r, txt.color.g, txt.color.b, 0);
+        yield return null;
+
+        while (txt.color.a < 1.0f)
+        {
+            txt.color = new Color(txt.color.r, txt.color.g, txt.color.b, txt.color.a + (Time.deltaTime / t));
+            yield return null;
+        }
+    }
+
+    public void WhenButtonPressed()
+    {
+        gameObject.SetActive(true);
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 1. **AnimatieGlisare** - https://www.youtube.com/watch?v=L3ePQSttC2s
 2. **IndicatorSanatate** - https://www.youtube.com/watch?v=Zdz6NFnfFwA
+3. **EcranNumaratoareInversa** - https://www.youtube.com/watch?v=vio7nzu0sBg
